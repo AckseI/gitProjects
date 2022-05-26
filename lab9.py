caefec1e32a8a75fa8af2b9928a7a7dc0dcf492c
AckseI <36826218+AckseI@users.noreply.github.com>
2022-05-26 16:30:59 +0300

    lab9

--- lab9.py
+++ lab9.py
@@ -1,10 +1,24 @@
 def searchName(f, search):
-    if search in f:
-        return f
+    l = 0
+    r = len(f) - 1
+    while (l<=r):
+        mid = (l + r) // 2
+        res = (search == f[mid])
+        print(res)
+        if (res == 0):
+            return mid - 1
+        if (res > 0):
+            l = mid + 1
+        else:
+            r = mid - 1
+    return None
 
 search = input("Введите фамилию: ")
 f = open("baza tel numbers.txt", encoding='utf-8').readlines()
-f = [line.split() for line in f]
-f.sort()
-index = searchName(f, search)
-print(f[index][1])
+f1 = [line.split() for line in f]
+f1.sort()
+result = searchName(f1, search)
+if (result == None):
+    print("Такой фамилии нет в списке")
+else:
+    print(f1[result])
