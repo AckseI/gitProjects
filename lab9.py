1bf308d0fc41674b13c0d05051cc331f04f9bf35
AckseI <egortrifo@gmail.com>
2022-06-02 19:48:30 +0300

    Лаба9 сделана

--- lab9.py
+++ lab9.py
@@ -1,10 +1,22 @@
-def searchName(f, search):
-    if search in f:
-        return f
+def searchName(arr, search):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        middle = (start + end) // 2
+        midpoint = arr[middle][0]
+        if midpoint > search:
+            end = middle - 1
+        elif midpoint < search:
+            start = middle + 1
+        else:
+            return middle
+
 
 search = input("Введите фамилию: ")
-f = open("baza tel numbers.txt", encoding='utf-8').readlines()
-f = [line.split() for line in f]
-f.sort()
-index = searchName(f, search)
-print(f[index][1])
+f = open("output.txt", encoding='utf-8').readlines()
+f1 = [line.split() for line in f]
+result = searchName(f1, search)
+if (result == None):
+    print("Такой фамилии нет в списке")
+else:
+    print(f1[result][1])
---
+++ tel.py
@@ -0,0 +1,16 @@
+def find(L, target):
+    start = 0
+    end = len(L) - 1
+    while start <= end:
+        middle = (start + end)// 2
+        midpoint = L[middle]
+        if midpoint > target:
+            end = middle - 1
+        elif midpoint < target:
+            start = middle + 1
+        else:
+            return midpoint
+
+L = ["Brian", "Joe", "Lois", "Meg", "Peter", "Stewie"]
+L = sorted(L)
+print(find(L, "Lois"))
